8ee428079c14723ce8ea598f11909751c9230122
leoliuyt <leoliuyt@sina.cn>
2018-03-31 17:39:25 +0800

    ignore

--- debug-objc/main.m
+++ debug-objc/main.m
@@ -12,6 +12,9 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        NSObject *objc = [NSObject new];
+        
+        [[NSRunLoop currentRunLoop] run];
     }
     return 0;
 }
